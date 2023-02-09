ec54bd95474e8c8f7b15401d91afcf2497d14ed6
Bouchedoub Ramzi <ios191169@gmail.com>
2023-02-09 20:41:39 +0100

    Initial Commit

---
+++ ImageGeneratorAi/ImageGeneratorAiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageGeneratorAiApp.swift
+//  ImageGeneratorAi
+//
+//  Created by Bouchedoub Ramzi on 9/2/2023.
+//
+
+import SwiftUI
+
+@main
+struct ImageGeneratorAiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
